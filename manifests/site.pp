b3ae0c6f68d1d42db1999228520ff1bbf784c452
JDUNA <99733395+Norrie37@users.noreply.github.com>
2022-04-03 13:43:43 +1000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,13 @@
 node default {
-  file { '/root/README':
-    ensure  => file,
-    content => 'This is a readme',
-    owner   => root,
-  }
+
 }
 
 node "oci-pmaster.subnet04030040.vcn04030040.oraclevcn.com" {
   include role::master_server
+  file { '/root/README':
+    ensure  => file,
+    content => $fqdn,
+  }
 }
 
 node /^web/ {
